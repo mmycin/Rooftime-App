9cea2a87db93f80da7e462e0e034f1d5929bfe7a
Tahcin Ul Karim Mycin <mycin.mit@gmail.com>
2025-03-07 22:45:12 +0600

    Login Expiry added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,37 @@
 <script>
-	import { goto } from "$app/navigation";
-	import Copyright from "$lib/components/Layouts/Copyright.svelte";
-	import Login from "$lib/components/Login.svelte";
+	import { goto } from '$app/navigation';
+	import Copyright from '$lib/components/Layouts/Copyright.svelte';
+	import Login from '$lib/components/Login.svelte';
+	import { IsExpired } from '$lib/Utils/Token';
 
-    let isLoggedIn = true;
+	let isLoggedIn = true;
 
-    $: {
-        try {
-            const token = localStorage.getItem('token');
-            if (token !== null) {
-                isLoggedIn = true;
-                goto('/dashboard');
-            } else {
-                isLoggedIn = false;
-            }
-        } catch (error) {
-            console.log(error);
-        }
+	$: {
+		try {
+			const token = localStorage.getItem('token');
+			if (token !== null) {
+				if (IsExpired(token)) {
+					localStorage.removeItem('token');
+					isLoggedIn = false;
+					goto('/');
+				} else {
+					isLoggedIn = true;
+					goto('/dashboard');
+				}
+			} else {
+				isLoggedIn = false;
+			}
+		} catch (error) {
+			console.log(error);
+		}
 
-        console.log(isLoggedIn);
-    }
+		console.log(isLoggedIn);
+	}
 </script>
 
 <main>
-    {#if !isLoggedIn}
-        <Login />
-        <Copyright />   
-    {/if}
+	{#if !isLoggedIn}
+		<Login />
+		<Copyright />
+	{/if}
 </main>
